60f1dc410f50a4cc1e7ec9b22fbba6a4d90e3480
nivedita-how <nivedita.dighe2@zensar.com>
2025-10-20 19:30:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region  = "us-east-2"
+  region  = "us-west-2"
 }
 
 terraform {
   backend "s3" {
-  bucket = "ua-terraform-state1"
+  bucket = "ua-terraform-state2"
   key = "dev/ec2-app2/terraform.tfstate"
   region = "us-west-2"
   }
