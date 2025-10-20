27635a879ce4b1515a491c7ba7bbf4e2eaf68f10
nivedita-how <nivedita.dighe2@zensar.com>
2025-10-20 14:52:13 +0530

    Create ec2.tf
    creates an ec2 micro instance on dev

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "main" {
+  ami           = "ami-001089eb624938d9f"
+  instance_type = "t2.micro"
+  count         = 2
+
+  tags = {
+    Name = "IaC-Test"
+    Env = "dev"
+  }
+}
